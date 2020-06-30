5f72e3970f918191a4f738abcc63f2f60ddefb23
elegant-delimiter-3950 <kinnari1022@yahoo.com>
2020-06-30 18:44:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ var funkyFunction = function outsideFunction() {
    return "FUNKY!"
  }
 }
+console.log()
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
